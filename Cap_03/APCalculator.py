05c85cd729360aaa238e01a8bf77b29b3bab8a25
DaltonAPSantos <dalton.a.p2211@gmail.com>
2025-03-22 16:57:35 -0300

    cod01

---
+++ Cap_03/APCalculator.py
@@ -0,0 +1,9 @@
+print ("Bem vindo ao Calculador de Área de Paralelogramo")
+
+base = float(input('Digite o valor do comprimento da base: '))
+alt = float(input('Digite o valor da altura: '))
+
+area = base * alt
+
+print(area)
+
